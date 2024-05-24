f1e645298ad721e4fc085c6770f66c3680f2f4e3
GeroleoErickson <ericksongeroleo@gmail.com>
2024-05-24 17:02:23 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var navLinks = document.getElementById("sidebar")
+    function showMenu(){
+        sidebar.style.right = "-0.1rem"
+    }
+    function hideMenu(){
+        sidebar.style.right = "-17rem"
+    }
