41b937e33a3478161c708f88a18762e43731bd27
GeroleoErickson <ericksongeroleo@gmail.com>
2024-05-29 17:06:06 +0800

    14th commit
    add cart.html, update css, add darkmode feature

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 var navLinks = document.getElementById("sidebar")
     function showMenu(){
         sidebar.style.right = "-0.1rem"
@@ -5,3 +7,49 @@ var navLinks = document.getElementById("sidebar")
     function hideMenu(){
         sidebar.style.right = "-17rem"
     }
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleSwitch = document.getElementById('toggleDark').querySelector('input');
+
+    if (localStorage.getItem('dark-mode') === 'enabled') {
+        document.body.classList.add('dark-theme');
+        toggleSwitch.checked = true;
+    }
+    toggleSwitch.addEventListener('change', function() {
+        if (toggleSwitch.checked) {
+            document.body.classList.add('dark-theme');
+            localStorage.setItem('dark-mode', 'enabled');
+        } else {
+            document.body.classList.remove('dark-theme');
+            localStorage.removeItem('dark-mode');
+        }
+    });
+});
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const quantities = document.querySelectorAll('.quantity');
+
+    quantities.forEach(quantity => {
+        const incrementButton = quantity.querySelector('.increment');
+        const decrementButton = quantity.querySelector('.decrement');
+        const quantityDisplay = quantity.querySelector('.quanti');
+
+        incrementButton.addEventListener('click', function() {
+            let currentQuantity = parseInt(quantityDisplay.textContent, 10);
+            quantityDisplay.textContent = currentQuantity + 1;
+        });
+
+        decrementButton.addEventListener('click', function() {
+            let currentQuantity = parseInt(quantityDisplay.textContent, 10);
+            if (currentQuantity > 0) {
+                quantityDisplay.textContent = currentQuantity - 1;
+            }
+        });
+    });
+});
